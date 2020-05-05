df3cbd6315ba877a42d1b1517e1217f86a992cab
ALi <s.arme@ya.ru>
2020-05-06 00:33:08 +0430

    implement miniquery instance methods

--- src/index.js
+++ src/index.js
@@ -1,10 +1,61 @@
-const defaultAwesomeFunction = (name) => {
-  const returnStr = `I am the Default Awesome Function, fellow comrade! - ${name}`;
-  return returnStr;
-};
+class MinijQuery {
+  constructor({ element }) {
+    this.element = element
+  }
 
-const awesomeFunction = () => 'I am just an Awesome Function';
+  addClass(classes) {
+    this.element.className.concat(` ${ a.classes }`);
+  }
 
-export default defaultAwesomeFunction;
+  attr(attrName) {
+    return this.element[attrName];
+  }
 
-export { awesomeFunction };
+  children() {
+    return this.element.children;
+  }
+
+  click(callback) {
+    return this.element.onclick = callback;
+  }
+
+  empty(callback) {
+    Array(this.element.children).map(value => {
+      this.element.removeChild(value);
+    }, this)
+  }
+
+  fadeToggle(duration_time_second) {
+    let i = this.element.style.opacity;
+    let j = this.element.style.transition;
+    if (i === "" || i === 1) {
+      j = `all ${ duration_time_second }s ease```;
+      i = 0;
+    } else {
+      j = `all ${ duration_time_second }s ease`;
+      i = 1;
+    }
+  }
+
+  hasClass(className) {
+    let i = this.element.className;
+    let r = i.match(className);
+    return r !== null;
+  }
+
+  hover(callback) {
+    this.element.onmouseenter = callback;
+    this.element.onmouseleave = callback;
+  }
+
+  removeAttr(attrName) {
+    this.element.removeAttribute(attrName);
+  }
+
+  toggleClass(className) {
+    this.element.classList.toggle(className);
+  }
+
+}
+
+export { MinijQuery };
