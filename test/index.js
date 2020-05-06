96da7cbc271ba003b18465bcec072f4dea1d4865
ALi <s.arme@ya.ru>
2020-05-06 12:00:18 +0430

    implement testing for Mini jQuery class only for toggling classes

--- test/index.js
+++ test/index.js
@@ -1,14 +1,15 @@
 import { assert } from 'chai';
-import defaultAwesomeFunction, { awesomeFunction } from '../src';
+import { JSDOM } from 'jsdom';
+import { describe } from 'mocha';
+import MinijQuery from '../src/index';
 
-describe('Awesome test.', () => {
-  it('should test default awesome function', () => {
-    const expectedVal = 'I am the Default Awesome Function, fellow comrade! - Dinesh';
-    assert(defaultAwesomeFunction('Dinesh') === expectedVal, 'Default not awesome :(');
-  });
-
-  it('should test awesome function', () => {
-    const expectedVal = 'I am just an Awesome Function';
-    assert(awesomeFunction() === expectedVal, 'Named awesome :(');
+describe('test toggle class.', () => {
+  it('should test toggle function', () => {
+    const { document } = new JSDOM().window;
+    const i = document.querySelector('body');
+    const j = new MinijQuery(i);
+    j.toggleClass('test');
+    const expectedVal = 'test';
+    assert(i.className === expectedVal, 'Default not awesome :(');
   });
 });
